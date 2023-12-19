1ba46c05af662a470c99c3f652fc4b0edc4af209
Guilherme China <echina725@gmail.com>
2023-12-19 12:02:39 -0300

    feat: iteration over each unique device from the list && close notification modal feature

--- automation.py
+++ automation.py
@@ -2,13 +2,17 @@ from selenium import webdriver;
 from selenium.webdriver.chrome.service import Service;
 from selenium.webdriver.common.by import By;
 from selenium.webdriver.common.keys import Keys;
-from utils import get_element_after_rendered;
+from utils import get_element_after_rendered, finish_os;
+import pandas as pd
+import numpy as np
 
 import time
 
+data = pd.read_excel("./data.xlsx");
+rgis = data["rgi"].unique();
 
 
-service = Service(executable_path=r"chromedriver.exe");
+service = Service(executable_path=r"./chromedriver");
 driver = webdriver.Chrome(service=service);
 
 driver.get("https://aguasdejoinville.com.br/sansys/index.html");
@@ -23,28 +27,49 @@ submit_login = driver.find_element(By.CSS_SELECTOR, "[name='btn_logar']");
 name_input.clear();
 password_input.clear();
 
+# login form
 name_input.send_keys("vogel.000001");
 password_input.send_keys("Caj323");
 submit_login.click();
 time.sleep(0.3);
 submit_login.click();
-
+OS_STATUS_POSITION_IN_TABLE = 3;
 menu_item = get_element_after_rendered(driver, elem_name="button[name='menuItem']",)
-menu_item.click();
 
-attendence_button = get_element_after_rendered(driver, elem_name="div[name='M00003']",)
-attendence_button.click();
+for i, rgi in enumerate(rgis):
+    menu_item.click();
+    # from the first iteration, the link will be already open. No need to expand it again
+    if i == 0:
+        attendence_button = get_element_after_rendered(driver, elem_name="div[name='M00003']",)
+        attendence_button.click();
 
-start_attendence_link = get_element_after_rendered(driver, elem_name="Iniciar Atendimento",
-                                                   selector=By.PARTIAL_LINK_TEXT)
-start_attendence_link.click();
+    time.sleep(0.7);
+    start_attendence_link = get_element_after_rendered(driver, elem_name="a[href='#/interno/atendimento']")
+    start_attendence_link.click();
 
-search_rgi_field = get_element_after_rendered(driver, elem_name=".q-field__native.q-placeholder[name='search']");
-time.sleep(5);
-search_rgi_field.send_keys("00213910-3" + Keys.ENTER);
+    search_rgi_field = get_element_after_rendered(driver, elem_name=".q-field__native.q-placeholder[name='search']");
+    search_rgi_field.send_keys(rgi + Keys.ENTER); #ALERT
+    time.sleep(1);
+    # verify client garantee
+    try:
+        verify_client_warning_element = driver.find_element(By.CSS_SELECTOR, "button#notification-modal-ok");
+        verify_client_warning_element.click();
+        time.sleep(1);
+        continue;
+    except:
+        pass;
 
-see_service_orders = get_element_after_rendered(driver, ".menu--item > a[href*='ordens-de-servico']");
-see_service_orders.click();
+    see_service_orders = get_element_after_rendered(driver, ".menu--item > a[href*='ordens-de-servico']");
+    see_service_orders.click();
 
-time.sleep(3);
+    os_status = get_element_after_rendered(driver, "small[data-v-607cef2f]", selection_mode="all")\
+                                        [OS_STATUS_POSITION_IN_TABLE];
+
+    os_status_content = os_status.get_attribute("innerHTML").lower().strip();
+    if os_status_content == "encerrado - executado":
+        continue;
+    elif os_status_content == "pendente":
+        finish_os(driver, element_name=".q-checkbox__bg.absolute");
+
+time.sleep(5)
 driver.quit();
--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.support.ui import WebDriverWait;
 from selenium.webdriver.common.by import By;
 from selenium.webdriver.support import expected_conditions as EC;
+from selenium import webdriver;
 
-
-def get_element_after_rendered(driver, elem_name:str, selector:By = By.CSS_SELECTOR, timeout:int = 5):
+def get_element_after_rendered(driver:webdriver.Chrome, elem_name:str, selector:By = By.CSS_SELECTOR,
+                               timeout:int = 5, selection_mode:str = "single"):
     """
         Waits for an element to be rendered on the screen,
         and only then selects it.
@@ -15,7 +16,26 @@ def get_element_after_rendered(driver, elem_name:str, selector:By = By.CSS_SELEC
         Returns:
             element_in_html (WebElement) : a HTML element representation.
     """
+
+    find_by_switcher = {
+    "single": driver.find_element,
+    "all": driver.find_elements
+    }
+
+
     WebDriverWait(driver, timeout) \
         .until(EC.presence_of_element_located((selector, elem_name)));
-    element_in_html = driver.find_element(selector, elem_name);
+    element_in_html = find_by_switcher[selection_mode](selector, elem_name);
     return element_in_html;
+
+
+def finish_os(driver: webdriver.Chrome, element_name: str):
+    choosed_os = get_element_after_rendered(driver, element_name, selection_mode="all")[0];
+    choosed_os.click();
+
+    deletion_confirmation_btn = get_element_after_rendered(driver, "button[name='SHUT']");
+    deletion_confirmation_btn.click();
+
+    second_confirmation = driver.find_element(By.XPATH, '//div[contains(text(), "Encerrar Ordem de Serviço")]');
+    second_confirmation.click();
+    return;
